f928c122f0cf9587a714a19a4fa3b37b7fa0fa6a
Ashley K. W. Warren <28959300+wh33les@users.noreply.github.com>
2025-02-24 12:26:55 -0700

    Updated projects page.

--- Projects/tic-tac-toe/src/App.js
+++ Projects/tic-tac-toe/src/App.js
@@ -33,21 +33,20 @@ function Board({ xIsNext, squares, onPlay }) {
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {Array(3).fill(null).map((_, row) => (
+        <div className="board-row" key={row}>
+          {Array(3).fill(null).map((_, col) => {
+            const index = row * 3 + col;
+            return (
+              <Square
+                key={index}
+                value={squares[index]}
+                onSquareClick={() => handleClick(index)}
+              />
+            );
+          })}
+        </div>
+      ))}
     </>
   );
 }
@@ -70,16 +69,25 @@ export default function Game() {
 
   const moves = history.map((squares, move) => {
     let description;
-    if (move > 0) {
+    if (move === history.length - 1) {
+      description = "You are at move #" + move;
+    }
+    else if (move > 0) {
       description = "Go to move #" + move;
     } else {
       description = "Go to game start";
     }
-    return (
-      <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
-      </li>
-    );
+    if (move === history.length - 1) {
+      return (
+        <li key={move}>{description}</li>
+      );
+    } else {
+      return (
+        <li key={move}>
+          <button onClick={() => jumpTo(move)}>{description}</button>
+        </li>
+      );
+    }
   });
 
   return (
